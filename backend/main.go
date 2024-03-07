ae12b2bc0d05213762eebea87057b875c61c4dcf
Owen Shaw <123586721+oshaw1@users.noreply.github.com>
2024-03-07 16:10:08 +0000

    serve frontend

--- backend/main.go
+++ backend/main.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	// Serve frontend files
-	frontend := http.FileServer(http.Dir("frontend"))
+	frontend := http.FileServer(http.Dir("../frontend"))
 	http.Handle("/", frontend)
 
 	// API endpoint
